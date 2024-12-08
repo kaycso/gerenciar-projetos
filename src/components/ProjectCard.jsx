3ac6870cb8616b28b8bd758b1145c0a44ebe963a
kaycso <kaycsoliveira@hotmail.com>
2024-12-07 23:39:41 -0300

    feat: implement Projects page and ProjectCard component

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+// import { FaCircle } from "react-icons/fa";
+import { LuPen, LuTrash2 } from "react-icons/lu";
+import { FaCircle } from "react-icons/fa6";
+
+const ProjectCard = ({ name, budget, category }) => {
+  return (
+    <div className="flex flex-col gap-6 rounded border border-zinc-300 bg-white p-4 transition hover:shadow-md">
+      <h2 className="text-2xl font-bold">{name}</h2>
+      <div className="flex flex-col gap-2">
+        <p>
+          <span className="font-bold">Orçamento:</span> {budget}
+        </p>
+        <div className="flex items-center gap-1">
+          <FaCircle size={12} />
+          <p className="font">{category}</p>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <button className="flex items-center gap-2 rounded border border-zinc-300 bg-white px-4 py-2 font-medium duration-300 hover:bg-zinc-900 hover:text-amber-400">
+          <LuPen />
+          Editar
+        </button>
+        <button className="flex items-center gap-2 rounded border border-zinc-300 bg-white px-4 py-2 font-medium duration-300 hover:bg-zinc-900 hover:text-amber-400">
+          <LuTrash2 />
+          Excluir
+        </button>
+      </div>
+    </div>
+  );
+};
+
+ProjectCard.propTypes = {
+  name: PropTypes.string,
+  budget: PropTypes.number,
+  category: PropTypes.string,
+};
+
+export default ProjectCard;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import ProjectCard from "../components/ProjectCard";
+import Button from "../components/ui/Button";
+import { Link } from "react-router";
+
 const Projects = () => {
-  return <h1>Página Projects</h1>;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => setProjects(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <section className="mx-12 flex flex-col gap-6">
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-bold">Meus Projetos</h1>
+        <Link to="/newproject">
+          <Button>Criar Projeto</Button>
+        </Link>
+      </div>
+      <div className="flex flex-wrap gap-4 rounded">
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            name={project.name}
+            budget={project.budget}
+            category={project.category}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;

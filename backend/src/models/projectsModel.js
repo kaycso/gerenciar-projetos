9d54c3f1fe5f193874d73d7afa9c6e22d103528e
kaycso <kaycsoliveira@hotmail.com>
2024-12-12 15:02:13 -0300

    feat: modify project model to return coreect result in route fetching project

--- backend/src/models/projectsModel.js
+++ backend/src/models/projectsModel.js
@@ -19,12 +19,37 @@ const createProject = async (projectData) => {
 };
 
 const getProjectById = async (id) => {
-  const project = await sql`
-    SELECT p.* FROM projects p
-    LEFT JOIN services s ON s.project_id = p.id
+  const result = await sql`
+    SELECT 
+      p.*,
+      c.name as category_name,
+      s.id as service_id, s.title as service_title, s.description as service_description, s.cost as service_cost
+    FROM projects p
     LEFT JOIN categories c ON c.id = p.category_id
+    LEFT JOIN services s ON s.project_id = p.id
     WHERE p.id = ${id}
   `;
+
+  const project = {
+    id: result[0].id,
+    title: result[0].title,
+    budget: parseFloat(result[0].budget),
+    cost: parseFloat(result[0].cost),
+    category_id: result[0].category_id,
+    category_name: result[0].category_name,
+    services: [],
+  };
+
+  result.forEach((row) => {
+    const service = {
+      id: row.service_id,
+      title: row.service_title,
+      description: row.service_description,
+      cost: parseFloat(row.service_cost),
+    };
+
+    project.services.push(service);
+  });
 
   return project;
 };
